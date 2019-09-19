1dbf856c2f72c48ba4d7008636f3b30ae239b714
Moura <45687668+Moura00010001@users.noreply.github.com>
2019-09-19 18:45:31 -0300

    Add files via upload

--- tORD/testes/Main.java
+++ tORD/testes/Main.java
@@ -6,6 +6,8 @@ import algoritmos.CountingSort;
 import algoritmos.Dijkstra;
 import algoritmos.Huffman;
 import algoritmos.InsertionSort;
+import algoritmos.MergeSort;
+import algoritmos.QuickSort;
 import algoritmos.RadixSort;
 import algoritmos.SelectionSort;
 import estruturas.Codigo;
@@ -102,6 +104,8 @@ public class Main{
 		RadixSort rs = new RadixSort("num.1000.1.in");
 		InsertionSort is = new InsertionSort("num.1000.1.in");
 		SelectionSort ss = new SelectionSort("num.1000.1.in");
+		MergeSort ms = new MergeSort("num.1000.1.in");
+		QuickSort qs = new QuickSort("num.1000.1.in");
 		
 		long[] A = {15, 0, 3, 4, 17, 12, 1, 0, 5, 17, 2, 7, 6, 8, 0, 16, 4, 17, -50, -7, 5, -1, 17}, B = null;		
 		long[] C = {15, 0, 3, 4, 17, 12, 1, 0, 5, 17, 2, 7, 6, 8, 0, 16, 4, 17, -50, -7, 5, -1, 17};
@@ -113,10 +117,18 @@ public class Main{
 		long D[] = {15, 0, 3, 4, 17, 12, 1, 0, 5, 17, 2, 7, 6, 8, 0, 16, 4, 17, -50, -7, 5, -1, 17};		
 		SelectionSort.algoritmoSelectionSort(D);
 		
+		long E[] = {15, 0, 3, 4, 17, 12, 1, 0, 5, 17, 2, 7, 6, 8, 0, 16, 4, 17, -50, -7, 5, -1, 17};
+		MergeSort.algoritmoMergeSort(E, 0, E.length - 1);
+		
+		long F[] = {15, 0, 3, 4, 17, 12, 1, 0, 5, 17, 2, 7, 6, 8, 0, 16, 4, 17, -50, -7, 5, -1, 17};
+		QuickSort.algoritmoQuickSort2(F, 0, F.length - 1);
+		
 		CountingSort cs3 = new CountingSort("num.10000.4.in");
 		RadixSort rs2 = new RadixSort("num.100000.4.in");
 		InsertionSort is2 = new InsertionSort("num.100000.4.in");
 		SelectionSort ss2 = new SelectionSort("num.100000.4.in");
+		MergeSort ms2 = new MergeSort("num.100000.4.in");
+		QuickSort qs2 = new QuickSort("num.100000.4.in");
 				
 		System.out.println("Finish!");
 
